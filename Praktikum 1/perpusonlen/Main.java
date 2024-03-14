318503aeb11e1212d769d09393e97c2200ad1e32
Ivani <160981219+ShokuVan@users.noreply.github.com>
2024-03-14 13:25:43 +0700

    Delete Praktikum 1/perpusonlen directory

--- Praktikum 1/perpusonlen/Main.java
+++
@@ -1,9 +0,0 @@
-package perpusonlen;
-
-public class Main {
-    public static void main(String[] args) {
-        Tampilan tampilan = new Tampilan();
-        tampilan.tampilkanMenuUtama();
-    }
-}
-
